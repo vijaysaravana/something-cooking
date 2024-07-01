fbf65fe8665d0067de81ada91f30aaf78b4742d9
Vijay Saravana Jaishanker <jvs123456@gmail.com>
2024-06-30 18:47:14 -0700

    Simple server

--- src/main.py
+++ src/main.py
@@ -1,5 +1,24 @@
-def main():
-    print("Hello")
+# Python 3 server example
+from http.server import BaseHTTPRequestHandler, HTTPServer
 
-if __name__ == '__main__':
-    main()
+hostName = "localhost"
+serverPort = 8080
+
+class MyServer(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header("Content-type", "text/html")
+        self.end_headers()
+        self.wfile.write(bytes("<html><head><title>My Web Server</title></head>", "utf-8"))
+        self.wfile.write(bytes(f"<p>Request: {self.path}</p>", "utf-8"))
+        self.wfile.write(bytes("<body><p>This is an example web server.</p></body></html>", "utf-8"))
+
+if __name__ == "__main__":
+    webServer = HTTPServer((hostName, serverPort), MyServer)
+    print(f"Server started at http://{hostName}:{serverPort}")
+    try:
+        webServer.serve_forever()
+    except KeyboardInterrupt:
+        pass
+    webServer.server_close()
+    print("Server stopped.")

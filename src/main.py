5f6eeda6040042898321099e3bfd60874acf233b
Vijay Saravana Jaishanker <jvs123456@gmail.com>
2024-06-30 18:59:46 -0700

    chore: use flask;

--- src/main.py
+++ src/main.py
@@ -1,24 +1,10 @@
-# Python 3 server example
-from http.server import BaseHTTPRequestHandler, HTTPServer
+from flask import Flask
 
-hostName = "localhost"
-serverPort = 8080
+app = Flask(__name__)
 
-class MyServer(BaseHTTPRequestHandler):
-    def do_GET(self):
-        self.send_response(200)
-        self.send_header("Content-type", "text/html")
-        self.end_headers()
-        self.wfile.write(bytes("<html><head><title>My Web Server</title></head>", "utf-8"))
-        self.wfile.write(bytes(f"<p>Request: {self.path}</p>", "utf-8"))
-        self.wfile.write(bytes("<body><p>This is an example web server.</p></body></html>", "utf-8"))
+@app.route("/")
+def hello():
+    return "<html><head><title>My Web Server</title></head><body><p>This is an example web server.</p></body></html>"
 
 if __name__ == "__main__":
-    webServer = HTTPServer((hostName, serverPort), MyServer)
-    print(f"Server started at http://{hostName}:{serverPort}")
-    try:
-        webServer.serve_forever()
-    except KeyboardInterrupt:
-        pass
-    webServer.server_close()
-    print("Server stopped.")
+    app.run(host="0.0.0.0", port=8080)
